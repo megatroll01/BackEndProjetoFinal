4507c7de89abbac69a4de428f22e2fcbe9d76848
Yan Medeiros <medeiros.yan@live.com>
2024-08-17 16:28:24 -0300

    first commit

---
+++ myworkmanager_back/src/main/java/com/my/work/manager/myworkmanager_back/repository/VendaRepository.java
@@ -0,0 +1,11 @@
+package com.my.work.manager.myworkmanager_back.repository;
+
+import com.my.work.manager.myworkmanager_back.model.VendaObj;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface VendaRepository extends JpaRepository<VendaObj, Long> {
+    List<VendaObj> findByDataVendaBetween(LocalDate startDate, LocalDate endDate);
+}
